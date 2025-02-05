9276eb9607a156f2087a854670d0c12e092f491c
Usman Akram <ru0300usman@gmail.com>
2025-02-06 00:51:45 +0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Search from './pages/Search'
+import Listing from './pages/Listing'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+             
+    </Routes>
+    </BrowserRouter>)
+}
+
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import { FaSearch } from "react-icons/fa"
+
+
+
+export default function Header() {
+  return (
+    <div>Header</div>
+  )
+}
