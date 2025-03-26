e49f9932ab435bd94bfd7a73e6397d03c4fd3baf
Usman Akram <ru0300usman@gmail.com>
2025-03-26 09:47:53 +0500

    add redux toolkit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,23 @@
-import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Search from './pages/Search'
-import Listing from './pages/Listing'
-import Profile from './pages/Profile'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import About from './pages/About'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
-import React from 'react'
-import Header from './components/Header'
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profile" element={<Profile />} />
-             
-    </Routes>
-    </BrowserRouter>)
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
